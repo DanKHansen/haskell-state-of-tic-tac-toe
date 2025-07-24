407a424bd9638422bf4187857194a1d37076a3d5
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-24 14:12:47 +0200

    refactor

--- src/StateOfTicTacToe.hs
+++ src/StateOfTicTacToe.hs
@@ -5,14 +5,14 @@ import Data.List (transpose)
 data GameState = WinX | WinO | Draw | Ongoing | Impossible deriving (Eq, Show)
 
 gameState :: [String] -> GameState
-gameState board
-  | invalidState = Impossible
+gameState b
+  | invalid = Impossible
   | win 'X' = WinX
   | win 'O' = WinO
   | turns 'O' + turns 'X' == 9 = Draw
   | otherwise = Ongoing
   where
-    turns c = sum $ map (length . filter (== c)) board
-    win c = [c, c, c] `elem` (board ++ transpose board ++ diagonals)
-    diagonals = [[board !! i !! i | i <- [0 .. 2]], [board !! i !! (2 - i) | i <- [0 .. 2]]]
-    invalidState = turns 'O' > turns 'X' || turns 'X' > turns 'O' + 1 || (win 'X' && win 'O')
+    turns c = sum $ map (length . filter (== c)) b
+    win c = [c, c, c] `elem` (b ++ transpose b ++ diags)
+    diags = [[b !! i !! i | i <- [0 .. 2]], [b !! i !! (2 - i) | i <- [0 .. 2]]]
+    invalid = turns 'O' > turns 'X' || turns 'X' > turns 'O' + 1 || (win 'X' && win 'O')
