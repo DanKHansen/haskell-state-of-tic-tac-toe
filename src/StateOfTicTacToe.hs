99c9d882b15486ae95a45116e15c7f44f52bb240
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-24 13:31:33 +0200

    solution

--- src/StateOfTicTacToe.hs
+++ src/StateOfTicTacToe.hs
@@ -1,6 +1,16 @@
-module StateOfTicTacToe (gameState, GameState(..)) where
+module StateOfTicTacToe (gameState, GameState (..)) where
+
+import Data.List (transpose)
 
 data GameState = WinX | WinO | Draw | Ongoing | Impossible deriving (Eq, Show)
 
 gameState :: [String] -> GameState
-gameState board = error "You need to implement this function."
+gameState board
+  | cs 'O' > cs 'X' || cs 'X' > (cs 'O' + 1) || (win 'X' && win 'O') = Impossible
+  | win 'X' = WinX
+  | win 'O' = WinO
+  | cs 'O' + cs 'X' == 9 = Draw
+  | otherwise = Ongoing
+  where
+    cs c = sum $ map (length . filter (== c)) board
+    win c = [c, c, c] `elem` board || [c, c, c] `elem` transpose board || [c, c, c] == [head (head board), board !! 1 !! 1, last (last board)] || [c, c, c] == [last (head board), board !! 1 !! 1, head (last board)]
