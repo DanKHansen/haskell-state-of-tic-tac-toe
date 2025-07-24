6f59b241a6677791d165a00eba66c8dd9849858a
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-24 14:03:35 +0200

    refactor

--- src/StateOfTicTacToe.hs
+++ src/StateOfTicTacToe.hs
@@ -9,11 +9,10 @@ gameState board
   | invalidState = Impossible
   | win 'X' = WinX
   | win 'O' = WinO
-  | allFilled = Draw
+  | turns 'O' + turns 'X' == 9 = Draw
   | otherwise = Ongoing
   where
-    counts c = sum $ map (length . filter (== c)) board
+    turns c = sum $ map (length . filter (== c)) board
     win c = [c, c, c] `elem` (board ++ transpose board ++ diagonals)
     diagonals = [[board !! i !! i | i <- [0 .. 2]], [board !! i !! (2 - i) | i <- [0 .. 2]]]
-    invalidState = counts 'O' > counts 'X' || counts 'X' > counts 'O' + 1 || (win 'X' && win 'O')
-    allFilled = counts 'O' + counts 'X' == 9
+    invalidState = turns 'O' > turns 'X' || turns 'X' > turns 'O' + 1 || (win 'X' && win 'O')
